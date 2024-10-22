04b02a2494dc7b93a6e6dbfd61a5fb5308707bfa
AnubisReal <44744582+AnubisReal@users.noreply.github.com>
2024-10-23 00:59:27 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v1.3'; // Cambia manualmente el nombre de la caché para forzar la actualización
+const CACHE_NAME = 'v1.4'; // Cambia manualmente el nombre de la caché para forzar la actualización
 const urlsToCache = [
     '/',
     '/index.html',
